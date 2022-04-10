66e9b9c0ee605acdab0958f90c95dcc6269ff137
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-04-10 19:20:32 +0300

    commit run_app

--- RestApi/run_app.py
+++ RestApi/run_app.py
@@ -1,10 +1,12 @@
 from blueprint.user import user
 from blueprint.auditorium import auditorium
+from blueprint.access import access
 from flask import Flask
 
 app = Flask(__name__)
 app.register_blueprint(user)
 app.register_blueprint(auditorium)
+app.register_blueprint(access)
 
 if __name__ == "__main__":
     app.run(debug=True)
