c848d755837c8a4f04f32076d39f5cf71f5ab659
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-04-11 10:46:37 +0300

    commit access

--- RestApi/tests/test_access.py
+++ RestApi/tests/test_access.py
@@ -1,15 +1,16 @@
 from run_app import app
-from models import Session, User, Auditorium, Access
+from models import Session, User, Auditorium
+from datetime import datetime
 import unittest
 
 
 class TestAccess(unittest.TestCase):
     """_summary_
-
-    Args:
-        unittest (_type_): _description_
+    Testing all access methods.
+    I define two methods : setUp() and tearDown() (setUp - adds all prerequisite steps, tearDown - clean-up all steps).
+    After that tests : POST, GET, PUT and DELETE are tested below.
     """
-    
+
     def setUp(self):
         self.app_context = app.app_context()
         self.app_context.push()
@@ -54,12 +55,125 @@ class TestAccess(unittest.TestCase):
         access = {
             "auditorium_id": int(created_auditorium.json["id"]),
             "user_id": int(created_user.json["id"]),
-            "start":"2022-5-28 11:00:00",
-            "end": "2022-5-28 12:30:00"
+            "start":"2023-5-28 11:00:00",
+            "end": "2023-5-28 12:30:00"
         }
         response = self.client.post(url_access, json=access)
         self.assertEqual(response.status_code, 200)
 
-        url_acess_delete = f'/access/{created_auditorium.json["id"]}'
-        response = self.client.delete(url_acess_delete)
+        expected_start = "2023-05-28T11:00:00"
+        actual_start = response.json["start"]
+        self.assertEqual(actual_start, expected_start)
+
+        url_access_delete = f'/access/{created_auditorium.json["id"]}'
+        response = self.client.delete(url_access_delete)
         self.assertEqual(response.status_code, 200)
+    
+    def test_create_error_time(self):
+        created_user = self.client.post(self.url_user, json=self.user)
+        created_auditorium = self.client.post(self.url_auditorium, json=self.auditorium)
+
+        url_access = '/access'
+        accesses = [{
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": ".....",
+            "start":"2023-5-28 11:00:00",
+            "end": "2023-5-28 12:30:00"
+        },
+        {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2018-5-28 11:00:00",
+            "end": "2018-5-28 12:30:00"
+        },
+        {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 11:00:00",
+            "end": "2023-5-28 11:30:00"
+        },
+        {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 11:00:00",
+            "end": "2023-5-28 19:00:00"
+        }]
+        for i in range(len(accesses)):
+            response = self.client.post(url_access, json=accesses[i])
+            self.assertEqual(response.status_code, 400)
+    
+    def test_create_error_reservation(self):
+        created_user = self.client.post(self.url_user, json=self.user)
+        created_auditorium = self.client.post(self.url_auditorium, json=self.auditorium)
+
+        url_access = '/access'
+        access = {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 11:00:00",
+            "end": "2023-5-28 12:30:00"
+        }
+        self.client.post(url_access, json=access)
+
+        accesses = [{
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 10:00:00",
+            "end": "2023-5-28 13:30:00"
+        },
+        {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 11:10:00",
+            "end": "2023-5-28 12:20:00"
+        },
+        {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 9:00:00",
+            "end": "2023-5-28 11:30:00"
+        },
+        {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 11:30:00",
+            "end": "2023-5-28 14:00:00"
+        }]
+        for i in range(len(accesses)):
+            response = self.client.post(url_access, json=accesses[i])
+            self.assertEqual(response.status_code, 403)
+        
+        url_access_delete = f'/access/{created_auditorium.json["id"]}'
+        response = self.client.delete(url_access_delete)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_retrieve(self):
+        created_user = self.client.post(self.url_user, json=self.user)
+        created_auditorium = self.client.post(self.url_auditorium, json=self.auditorium)
+
+        url_access = '/access'
+        access = {
+            "auditorium_id": int(created_auditorium.json["id"]),
+            "user_id": int(created_user.json["id"]),
+            "start":"2023-5-28 11:00:00",
+            "end": "2023-5-28 12:30:00"
+        }
+        self.client.post(url_access, json=access)
+
+        url_access_get = f'/access/{created_user.json["id"]}'
+        response = self.client.get(url_access_get)
+        self.assertEqual(response.status_code, 200)
+
+        url_access_delete = f'/access/{created_auditorium.json["id"]}'
+        response = self.client.delete(url_access_delete)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_retrieve_error(self):
+        url_access_get = '/access/100000'
+        response = self.client.get(url_access_get)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_delete_error(self):
+        url_access_delete = '/access/100000'
+        response = self.client.delete(url_access_delete)
+        self.assertEqual(response.status_code, 404)
